f6655da3ea1d8fcf5c54734986892999afc5b216
BekbolotM <bekbolot93osh@gmail.com>
2022-06-29 20:51:13 +0600

    problem4

---
+++ problem4.py
@@ -0,0 +1,18 @@
+a = 17*3
+b = 12*5
+c = 12**3
+d = 13*7
+e = 4**5
+f = 512+512
+if a > b:
+  print('A bolshe B')
+else:
+  print('B bolshe A')
+if c > d:
+  print('C bolshe B')
+else:
+  print('B bolshe C')
+if e > f:
+  print('E bolshe F')
+else:
+  print('F bolshe E')
