5aff94ea814e3d016b3ba9a755045d525bc1756a
BekbolotM <bekbolot93osh@gmail.com>
2022-06-30 19:23:54 +0600

    Зынк

---
+++ uslovia/problem2.py
@@ -0,0 +1,9 @@
+a = int(input('Chislo a: '))
+b = int(input('Chislo b: '))
+c = int(input('Chisli c: '))
+if a > b and a > c:
+  print('A bolshe')
+elif b > a and b > c:
+  print('B bolshe')
+elif c > a and c > b:
+  print('C bolshe')
