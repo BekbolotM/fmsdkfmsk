0c8123c217c9dbe330b5cd52aa23692e3ab424c5
BekbolotM <bekbolot93osh@gmail.com>
2022-06-30 20:27:28 +0600

    done

---
+++ uslovia/problem11.py
@@ -0,0 +1,10 @@
+a = 10
+b = 5
+if a > 0 and b > 0:
+  print('polojitelnye chisla')
+elif a < 0 and b > 0:
+  print('a otricatelnoe, b polojitelnoe')
+elif a > 0 and b < 0:
+  print('a polojitelnoe, b otricatelnoe')
+else:
+  print('pustota')
