07d9c5885f9ae11aac1de08cda5a64391729c5e4
BekbolotM <bekbolot93osh@gmail.com>
2022-06-30 19:51:32 +0600

    done

---
+++ uslovia/problem5.py
@@ -0,0 +1,13 @@
+a = int(input('Введите Ваше число: '))
+if a % 2 == 0:
+  print('chetnoe')
+else:
+  print('Ne chetnoe')
+if a % 3 == 0:
+  print('Delitcya bez ostatka')
+if a % 3 != 0:
+  print('Ne delitcya bez ostatka')
+if a**2 > 1000:
+  print('Bolshe 1000')
+else:
+  print('Menshe 1000')
