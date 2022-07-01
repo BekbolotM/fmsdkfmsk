2490af298f31193844b5c7f7c5ef03e1dd778352
BekbolotM <bekbolot93osh@gmail.com>
2022-07-01 19:23:14 +0600

    ispravil

--- Циклы/problem1.py
+++ Циклы/problem1.py
@@ -2,7 +2,7 @@ lang1 = 'Rust'
 
 languages = ['go', 'java', 'php', 'python', 'javascript', 'ruby']
 
-for i in lang1:
+for i in languages:
   if i == lang1:
     print('this languages is in list')
   
