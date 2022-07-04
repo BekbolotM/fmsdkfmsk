b744244a497f45005e323fb3e1f1e79809adc0fe
BekbolotM <bekbolot93osh@gmail.com>
2022-07-04 19:37:02 +0600

    ispravil

--- dict/problem1.py
+++ dict/problem1.py
@@ -1,3 +1,3 @@
-drinks = {}
-drinks['Coca-Cola', 'Sprite', 'Fanta'] = 'Напитки'
-print(drinks)
+menu = {'lagman': 120, 'plov': '120', 'borsh': 100}
+menu['Coca-Cola', 'Sprite', 'Fanta'] = 'drinks'
+print(menu)
