353e06a8bd12873966a89ea9eb2f3b5ce065124a
BekbolotM <bekbolot93osh@gmail.com>
2022-07-05 21:09:00 +0600

    done

--- generate/problem5.py
+++ generate/problem5.py
@@ -1,3 +1,6 @@
 names = ['Kairat', 'Erlan', 'Dastan', 'Artur', 'Bahtoolot', 'Azat', 'Bakyt']
 names.sort()
 print(names)
+#2
+names = ['Kairat', 'Erlan', 'Dastan', 'Artur', 'Bahtoolot', 'Azat', 'Bakyt']
+print(sorted(names))
