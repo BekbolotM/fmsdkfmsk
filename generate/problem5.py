3d54f8a9a45474caaed8567c40181d97808c48db
BekbolotM <bekbolot93osh@gmail.com>
2022-07-05 21:25:09 +0600

    dobavil version

--- generate/problem5.py
+++ generate/problem5.py
@@ -4,3 +4,7 @@ print(names)
 #2
 names = ['Kairat', 'Erlan', 'Dastan', 'Artur', 'Bahtoolot', 'Azat', 'Bakyt']
 print(sorted(names))
+#3
+names = ['Kairat', 'Erlan', 'Dastan', 'Artur', 'Bahtoolot', 'Azat', 'Bakyt']
+srt = [i for i in sorted(names)]
+print(srt)
